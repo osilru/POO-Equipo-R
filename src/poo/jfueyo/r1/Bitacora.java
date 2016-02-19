31559211ca72b8b7f89ac80660bd1573eedb8537
osilru <osilru1@hotmail.com>
2016-02-19 16:49:05 -0600

    Update Bitacora.java

--- src/poo/jfueyo/r1/Bitacora.java
+++ src/poo/jfueyo/r1/Bitacora.java
@@ -8,72 +8,109 @@ import java.util.*;
 
 /**
  * Clase que almacenará todas las transferencias o transacciones
+ *
  * @author A00509871
  */
 public class Bitacora {
+
     // Atributos
     /**
      * Se guardan en una lista las transferencias
      */
-    private List<Transferencia> listaDeTransferencias;    
-    
+    private List<Transferencia> listaDeTransferencias;
+
     /**
      * Constructor que inicializa la bitácora
      */
-    public Bitacora(){
-        this.listaDeTransferencias = new ArrayList<Transferencia>();        
+    public Bitacora() {
+        this.listaDeTransferencias = new ArrayList<Transferencia>();
     }
-    
+
     /**
-     * Cosntructor que inicializa la bitácora pasándole la lista de 
+     * Cosntructor que inicializa la bitácora pasándole la lista de
      * transferencia de alguna otra bitácora
-     * @param listaTransferencia 
+     *
+     * @param listaTransferencia
      */
-    public Bitacora (List<Transferencia> listaTransferencia){
+    public Bitacora(List<Transferencia> listaTransferencia) {
         this.listaDeTransferencias = listaTransferencia;
     }
-    
+
     /**
      * Método que agregará una transferencia a la lista
+     *
      * @param transferencia Es una transferencia que representa una transacción
      * o transferencia hecha
      * @return Si se agregó con éxito la transferencia
      */
-    public boolean agregarALista(Transferencia transferencia){
-        
+    public boolean agregarALista(Transferencia transferencia) {
+        return this.listaDeTransferencias.add(transferencia);
     }
-    
+
     /**
      * Método que elimina una transferencia de la lista
-     * @param transferencia Es la transferencia que se desea eliminar de la lista
+     *
+     * @param transferencia Es la transferencia que se desea eliminar de la
+     * lista
      * @return si se eliminó con éxito la transferencia
      */
-    public boolean eliminarDeLista(Transferencia transferencia){
-        
+    public boolean eliminarDeLista(Transferencia transferencia) {
+        return this.listaDeTransferencias.remove(transferencia);
     }
-    
+
     /**
      * Método que regresa la lista de transferencias de la bitácora
+     *
      * @return la lista de transferencias de la bitácora
      */
-    public List<Transferencia> getListaDeTransferencia(){
+    public List<Transferencia> getListaDeTransferencia() {
         return this.listaDeTransferencias;
     }
-    
+
     /**
-     * Es un método que regresa una cadena de caracteres con una descripción de las
-     * ultimas 5 transferencias
+     * Es un método que regresa una cadena de caracteres con una descripción de
+     * las ultimas 5 transferencias
+     *
      * @return un string con la descripción de las últimas 5 transacciones
      */
-    public String descripcionUltimos5(){
-        
+    public String descripcionUltimos5() {
+        String descripcion = "";
+
+        if (this.listaDeTransferencias.isEmpty()) {
+            descripcion = "==NO HAY MOVIMIENTOS==";
+        } else {
+            int i = 5;
+            if (this.listaDeTransferencias.size() < 5) {
+                i = this.listaDeTransferencias.size();
+            }
+            int auxiliar = this.listaDeTransferencias.size() - 1;
+
+            for (int e = 1; e <= i; e++) {
+                descripcion = descripcion + this.listaDeTransferencias.get(auxiliar).descripcionTransferencia() + "\n";
+                auxiliar = auxiliar - 1;
+                i = i - 1;
+            }
+        }
+        return descripcion;
     }
-    
+
     /**
      * Método que regresa la descripción de la bitácora
+     *
      * @return un string con la descripción de la bitácora
      */
-    public String descripcionBitacora(){
-        
+    public String descripcionBitacora() {
+        String descripcion = "";
+
+        if (this.listaDeTransferencias.isEmpty()) {
+            descripcion = "==NO HAY MOVIMIENTOS==";
+        } else {
+            int auxiliar = this.listaDeTransferencias.size();
+            for (int e = 1; e <= auxiliar; e++) {
+                auxiliar = auxiliar - 1;
+                descripcion = descripcion + this.listaDeTransferencias.get(auxiliar).descripcionTransferencia() + "\n";                
+            }
+        }
+        return descripcion;
     }
 }
